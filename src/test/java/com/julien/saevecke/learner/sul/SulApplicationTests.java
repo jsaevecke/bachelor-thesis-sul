fb9729d7dbcc3492cbaf8092ac11cff6a9349a69
Julien Saevecke <Julien.Saevecke@fino.digital>
2021-12-01 20:50:54 +0100

    add spring web, rabbitmq, learnlib, lombok dependencies to maven

---
+++ src/test/java/com/julien/saevecke/learner/sul/SulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julien.saevecke.learner.sul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
